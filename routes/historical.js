c22e40af4e8bb0244db66e48222626c0e8936bd0
Ryland <rylandmathews1@gmail.com>
2021-08-05 09:54:21 -0400

    phong api update

--- routes/historical.js
+++ routes/historical.js
@@ -7,4 +7,18 @@ router.route('/spreadhiststats').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/historicallongaprs').get((req, res) => {
+    standard('HistoricalLongAPRs').find()
+        .then(historicallongaprs => res.json(historicallongaprs))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/historicalshortaprs').get((req, res) => {
+    standard('HistoricalShortAPRs').find()
+        .then(historicalshortaprs => res.json(historicalshortaprs))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
+
+
