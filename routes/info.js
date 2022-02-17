4c7884ee859e428a3268a71289d1e6f2488d2c31
Ryland <rylandmathews1@gmail.com>
2022-02-17 11:00:07 -0500

    added api info call to get offered pools by dex

--- routes/info.js
+++ routes/info.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 let standard = require('../models/standard');
 
+router.route('/dexpooldict').get((req, res) => {
+    standard('dexpoolDICT').find()
+        .then(tokenDICT => res.json(tokenDICT))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 router.route('/tokendict').get((req, res) => {
     standard('tokenDICT').find()
         .then(tokenDICT => res.json(tokenDICT))
